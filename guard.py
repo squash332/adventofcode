71018669f41b4ea875e0734b106b9c2db3dd0d6f
squash332 <filip.hulak12@gmail.com>
2026-01-30 14:41:11 +0100

    2024-day-6-part-2

--- guard.py
+++ guard.py
@@ -1,4 +1,3 @@
-  
 #   ....#.....      ....#.....
 #   .........#      ....XXXXX#
 #   ..........      ....X...X.              41 distinct positions
@@ -8,7 +7,7 @@
 #   .#..^.....      .#XXxXXXX.
 #   ........#.      .XXXXXXX#.
 #   #.........      #XXXXXXX..
-#   ......#...      ......#X.. 
+#   ......#...      ......#X..
 
 # ^ - guard's starting position
 # # - obstacles, when obstacle is in front of guard : stop, turn right 90° and then keep moving
@@ -16,102 +15,136 @@
 # - need to count unique moves until guard leaves mapped area
 
 
+from copy import deepcopy
 from dataclasses import dataclass
+from pathlib import Path
+from xml.etree.ElementTree import tostring
 
 
-@dataclass
+@dataclass(frozen=True)
 class Point:
     x: int
     y: int
 
-    def up(self):
-        return Point(self.x - 1, self.y)
-    
-    def right(self):
-        return Point(self.x, self.y + 1)
-    
-    def down(self):
-        return Point(self.x + 1, self.y)
-    
-    def left(self):
-        return Point(self.x, self.y - 1)
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
 
-from pathlib import Path
+    def is_inside(self, dimensions: "Point") -> bool:
+        return 0 <= self.x < dimensions.x and 0 <= self.y < dimensions.y
+
 
 input_str = Path(__file__).parent / "pathing.txt"
-# data = str(input_str.read_text().strip().splitlines())
 arr = []
 
-DIRECTION = {
-    "up": Point.up,
-    "right": Point.right,
-    "down": Point.down,
-    "left":Point.left
-}
-
-TURN_RIGHT = {
-    "up": "right",
-    "right": "down",
-    "down": "left",
-    "left": "up"
-}
-
-guard = set()
+guard = Point(0,0)
 obstacle = set()
 
-for r, line in enumerate(input_str.read_text().strip().splitlines()):
-    print(line)
+data = input_str.read_text().strip().splitlines()
+
+DIRECTIONS = [Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1)]
+DIMENSIONS = Point(len(data), len(data[0]))
+
+for r, line in enumerate(data):
     for c, ch in enumerate(line.strip()):
         if ch == "^":
-            guard.add((r,c))
+            guard = Point(r, c)
         elif ch == "#":
-            obstacle.add((r,c))
+            obstacle.add(Point(r, c))
 
-guard_position = Point(*list(guard)[0]) # * unpacks the set object which is a tuple, because we need to move by modifying indexes
-# we will store visited places in a different set
+#print("dimensions: ", DIMENSIONS)
 
 visited_location = set()
-visited_location.add((guard_position.x, guard_position.y))
-direction = "up"
-while(0 <= guard_position.x < len(line) and 0<= guard_position.y < len(line)): # check if coordinates are inside of the length of the drawn map
-    next_position = DIRECTION[direction](guard_position)
-    #if next pos is an obstacle, turn right
-    if (next_position.x, next_position.y) in obstacle:
-        direction = TURN_RIGHT[direction]
-    #else keep ER GOIN & add that location to a set (because sets cannot contain duplicate values)
-    else:
-        guard_position = next_position
-        visited_location.add((guard_position.x, guard_position.y))
+#print(guard)
 
-print("nr of unique visited locations: ", len(visited_location)-1)
-#print("nr of unique visited locations: ", visited_location)
+def part_1(guard):
+    current_direction = 0 # Set current direction as going up
+    while True:  # check if coordinates are inside of the length of the drawn map
+        next_position = guard + DIRECTIONS[current_direction]
+        if not next_position.is_inside(DIMENSIONS):
+            break
 
-
-
-#print("positions of obstacles: ", obstacle) 
-
-#  ....#.....
-#  .........#
-#  ..........
-#  ..#.......
-#  .......#..
-#  ..........
-#  .#..^.....
-#  ........#.
-#  #.........
-#  ......#...
+        elif next_position in obstacle:
+            current_direction = (current_direction + 1) % len(DIRECTIONS)
+        else:
+            guard = next_position
+            visited_location.add(guard)
+    return visited_location
 
 
+
+
+def part_2(guard):
+    pathing = part_1(guard)
+    n_loops = 0 
     
+    for p in pathing:
+        if p == guard:
+            continue
 
-    
+        g = guard
+        visited = set()
+        with_added_obstacle = deepcopy(obstacle) # copying all obstacles
+        
+        #print("pathing : ", pathing, "\np: ",p )
+
+        with_added_obstacle.add(p)
+        
+        current_direction = 0
+        
+        while True: 
+            visited.add((g, current_direction))
+            next_position = g + DIRECTIONS[current_direction] 
+
+            if (next_position, current_direction) in visited:
+                
+                n_loops += 1
+                break
+            
+            if not next_position.is_inside(DIMENSIONS):
+                break
+
+            if next_position in with_added_obstacle: 
+                current_direction = (current_direction + 1) % len(DIRECTIONS) 
+                
+
+            else:
+                g = next_position
+    return n_loops
+
+print(f"Number of loops possible {part_2(guard)}")
 
 
 
 
+#  ..#...
+#  .....#
+#  ......
+#  .#^...
+#  ....#.
 
+# cycle counter
 
+# def print_map(visited, obstacles, guard):
+#     for x in range(DIMENSIONS.x):
+#         for y in range(DIMENSIONS.y):
+#             p = Point(x,y)
+#             if p == guard:
+#                 print("*", end="")
+#                 continue
+#             elif p in obstacles:
+#                 print("#", end="")
+#                 continue
 
+#             visited= False
+#             for v,_ in visited_location:
+#                 if p == v:
+#                     print("@", end="")
+#                     visited = True
+#                     break
 
+#             if not visited:
+#                 print(".", end="")
+#         print()
+#     print("\n\n")
 
-
+# #print("positions of obstacles: ", obstacle)

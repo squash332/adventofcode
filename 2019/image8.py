16cc6499f06b2dee025d18d4a0a1cb1108e1c304
squash332 <filip.hulak12@gmail.com>
2026-02-13 08:17:47 +0100

    day-8-part-2

--- 2019/image8.py
+++ 2019/image8.py
@@ -2,55 +2,49 @@ import math
 from pathlib import Path
 from collections import Counter
 
+
 input_str = Path(__file__).parent / "input.txt"
 data = input_str.read_text().strip().splitlines()
 
 
-
 IMAGE_WIDTH = 25
 IMAGE_HEIGHT = 6
-SPLIT = IMAGE_WIDTH * IMAGE_HEIGHT # 6
+SPLIT = IMAGE_WIDTH * IMAGE_HEIGHT 
+print(len(data[0]) // SPLIT) # NR OF LAYERS 
 
 string = data[0]
 
-images = dict ()
-
 val = "0"
 index = 0
-zeros = {}
-for i in range(0, len(string), SPLIT): # start 0 end 12 step 6
-    sum = 0
-    layer = string[i:i+SPLIT]
-    row = 0 
-    while row < IMAGE_HEIGHT:
-        start = row * IMAGE_WIDTH 
-        end = start + IMAGE_WIDTH
+print(string)
+layers = []
 
-        images[index] = layer[start:end]
-        for numbers in images[index]:
-            if val == numbers:
-                sum += 1
-                zeros[layer] = sum
-        row += 1
-        index += 1
-
-min_zeros = min(zeros.values())
-
-for k, v in zeros.items():
-    if v == min_zeros:
-        wanted_number = k
-
-wanted_list = Counter(wanted_number)
-
-for k in wanted_list.keys():
-    if k == '2':
-        first = int(wanted_list[k])
-    elif k == '1':
-        second = int(wanted_list[k])
-
-print(first*second)
+for i in range(0,len(string),SPLIT):
+    layers.append(string[i:i+SPLIT])
 
 
+
+final = list([ "2" ] * SPLIT )
+print("FINAL+++++++++++++++++++", final)
+
+for i in range(len(layers)-1,-1,-1):
+    for j in range(len(layers[i])):
+        if layers[i][j] in (["0", "1"]):
+            final[j] = layers[i][j]
+
+
+for i in range(0, SPLIT, IMAGE_WIDTH):
+    print("".join(final[i:i+IMAGE_WIDTH]).replace("0", " ").replace("1", "X"))
+
+
+        
+
+
+            
+
+# 0 is black
+# 1 is white
+# 2 is transparent
 
 
  

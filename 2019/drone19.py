9bc050405d3dc2d470ab02ed02fa406c4b5be2f0
squash332 <filip.hulak12@gmail.com>
2026-03-03 08:32:02 +0100

    day-19-part-2

--- 2019/drone19.py
+++ 2019/drone19.py
@@ -1,35 +1,38 @@
 from collections import defaultdict
-
+from dataclasses import dataclass
 import opcode2 as op
 from opcode2 import arr
+
 
 def get_grid(x,y):
     values = iter([x,y])
     return lambda: next(values)
 
-def draw_grid():
-    counter = 0
-    for x in range(50):
-        row = ""
-        for y in range(50):
-            match grid[(x,y)]:
-                case 1:
-                    row += "#"
-                    counter += 1
-                case 0:
-                    row += "."
 
-        print(row)
-    return counter
+def beam(x,y):
+    computer = op.IntCode(arr, get_grid(x,y))
+    return computer.run()
 
-
+x = 0
 grid = {}
-for x in range(50):
-    for y in range(50):
-        computer = op.IntCode(arr, get_grid(x,y))
-        result = computer.run()
-        grid[(x,y)] = result
+y = 500
 
+while True:
+    while beam(x,y) == 0:
+        x += 1
+        
+    # if (x, y) not in grid:
+    #     grid[(x,y)] = beam(x,y)
+        
+    # bottom left x,y 
+    # top right x + 99 y - 99
 
-print("part 1:", draw_grid())
+    #print top left (closest) x, y - 99
+    if beam(x + 99,y - 99) == 1:
+        x,y = x, y-99
+        break
+    y += 1
+
+print("part 2 =", x*10000 + y)
+
     
